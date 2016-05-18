d4a5d70723fff2510901cc54b0c5e6c136b4d5ce
Grant Colegate <blaknite@thelanbox.com.au>
2016-05-18 10:48:05 +0930

    Add method that delegates calls to presenter to the model

--- lib/blush/configuration.rb
+++ lib/blush/configuration.rb
@@ -1,9 +1,10 @@
 module Blush
   class Configuration
-    attr_accessor :accessor_name
+    attr_accessor :accessor_name, :helper_name
 
     def initialize
       @accessor_name = :presenter
+      @helper_name = :present
     end
   end
 end
--- lib/blush/has_presenter.rb
+++ lib/blush/has_presenter.rb
@@ -9,6 +9,10 @@ module Blush
       define_method(accessor_name || Blush.config.accessor_name) do
         @presenter ||= (self.blush_options[:class] || "#{self.model_name}Presenter".constantize).new(self)
       end
+
+      define_method(self.blush_options[:helper_name] || Blush.config.helper_name) do |method, *args, &block|
+        presenter.try(method, *args, &block) || send(method, *args, &block)
+      end
     end
   end
 end
